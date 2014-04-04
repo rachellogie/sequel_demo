00c291b615f60a38451dad90aa696a19cf58a536
rachellogie <rachel.logie1@gmail.com>
2014-04-03 19:37:20 -0600

    User can update attributes on a task

--- lib/tasks_repo.rb
+++ lib/tasks_repo.rb
@@ -1,24 +1,29 @@
 require 'sequel'
 
 class TasksRepo
-  def create(task)
-    db = Sequel.connect('postgres://gschool_user:password@localhost:5432/tasks_test')
 
-    db.create_table! :tasks do
+  def initialize
+    @db = Sequel.connect('postgres://gschool_user:password@localhost:5432/tasks_test')
+  end
+
+  def create(attributes)
+    @db.create_table! :tasks do
       primary_key :id
       String :description
       Boolean :completed, default: false
     end
-
-    tasks = db[:tasks]
-
-    tasks.insert(task)
-
+    tasks = @db[:tasks]
+    tasks.insert(attributes)
   end
 
   def all
-    db = Sequel.connect('postgres://gschool_user:password@localhost:5432/tasks_test')
-    tasks = db[:tasks]
+    tasks = @db[:tasks]
     tasks.to_a
   end
+
+  def update(id, attributes)
+    @db[:tasks].where('id = ?', id).update(attributes)
+  end
+
 end
+
--- spec/tasks_repo_spec.rb
+++ spec/tasks_repo_spec.rb
@@ -3,7 +3,6 @@ require 'tasks_repo'
 describe TasksRepo do
 
   it "can create tasks" do
-
     repo = TasksRepo.new
     description = "Do #{rand(2..20)} stuff"
     repo.create(description: description)
@@ -13,6 +12,19 @@ describe TasksRepo do
         {id: 1, description: description, completed: false}
       ]
     )
-
   end
+
+  it "updates the attributes of an item" do
+    repo = TasksRepo.new
+    id = repo.create(description: "hello")
+    repo.update(id, description: "bye", completed: true)
+    tasks = repo.all
+    expect(tasks).to eq(
+      [
+        {id: 1, description: "bye", completed: true}
+      ]
+    )
+  end
+
+
 end
